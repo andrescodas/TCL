64768962f1033118282f1e1a631f280a322e858a
Andres Codas <codas@laas.fr>
2010-02-01 23:07:26 +0100

    nuevo

---
+++ testLocMapFile.tcl
@@ -0,0 +1,39 @@
+source positionToFile.tcl
+source rfidLocMap.tcl
+
+set pi 3.1415
+set i 1
+
+#git test
+sdafas
+
+rfidLocMap::StartRobotParticles 2 -0.25 1
+rfidLocMap::StartOdometry 2 -0.25 1
+rfidLocMap::StartInput 
+
+set home $env(HOME)
+set path /simulation/results/
+set fileName rfidPositionSet.txt
+
+
+set rfidPosFile $home$path$fileName
+
+clearFile $rfidPosFile
+
+
+while { $i <= 323 } {
+		
+		
+		rfidLocMap::ActualizePositions
+	
+		getPositionRFIDLocMap positionRFID
+
+		set thetaRF [expr $positionRFID(theta)*180/$pi]
+	
+		position2File $i $rfidPosFile positionRFID
+
+		puts "RFID:\t$positionRFID(xRob)\t$positionRFID(yRob)\t$thetaRF"
+		
+		incr i
+
+} 
